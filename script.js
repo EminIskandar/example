afe8e434cd8bb73b317768a38a7a74ccee4c44bf
ElnaraSadiqli <elnaragk@code.edu.az>
2022-07-13 11:33:45 +0400

    "AddData"

--- script.js
+++ script.js
@@ -3,33 +3,67 @@ let inputName = document.querySelector(".input-name");
 let inputCount = document.querySelector(".input-count");
 let inputPrice = document.querySelector(".input-price");
 let inputCode = document.querySelector(".input-code");
+let trr = document.getElementsByTagName("tr");
 // Button
 let btn = document.querySelector("#btn-submit");
 // Table
 let table = document.querySelector(".table");
-let tbody = document.querySelector(".table-body");
+let ttbody = document.querySelector(".table-body");
 let tr;
 
+let bodyTable = [];
 
-console.log(eventListeners());
+
+eventListeners();
 
 function eventListeners() {
     btn.addEventListener("click", pushData);
+    btn.addEventListener("keypress",btnAdd )
 }
 
+AddData();
+function btnAdd (event) {
+    if (event.key == "Enter") {
+        btn.click();
 
-function pushData(event) {
-    event.preventDefault();
-    for (let id = 1; id < tbody.length; id++) {
+    }
+}
 
-        
-    tr = `<th scope="row">${id}</th>
+function AddData(event) {
+
+
+    for (let data of bodyTable) {
+
+        tr = `<th scope="row">${data.ids}</th>
     <td>${inputName.value}</td>
     <td>${inputCount.value}</td>
     <td>${inputCode.value}</td>`
 
-    tbody.insertAdjacentHTML("beforeend",tr);
+        ttbody.insertAdjacentHTML("beforeend", tr);
+
+
+
+        data.ids++;
+        event.preventDefault();
     }
 
- 
+
+    event.preventDefault();
+}
+
+function pushData(event) {
+
+
+    bodyTable.push({
+        "ids": bodyTable.length + 1,
+        "name": inputName.value,
+        "code": inputCode.value,
+        "count": inputCount.value,
+        "price": inputPrice.value
+    })
+
+
+
+    AddData();
+    event.preventDefault();
 }
